0c48f83f6fdd7e68e414fdb03a252e49ca5a6115
xeuthestoic <136644443+xeuthestoic@users.noreply.github.com>
2026-03-30 17:52:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 let cards = [];
 let currentIndex = 0;
 
